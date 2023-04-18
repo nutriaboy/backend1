1970cf32b94f8de8081c81898e4c7e3c14379d19
nutriaboy <fr.tapiam@gmail.com>
2023-04-18 01:45:01 -0400

    Se corrigieron errores en suscriptor.js

--- controllers/suscriptor.js
+++ controllers/suscriptor.js
@@ -1,1 +1,19 @@
-const { response } = require('exoress');
+const { response } = require('express');
+
+/*
+
+const obtenerSuscriptor = async(req, res = response ) => {
+
+    const { id } = req.params;
+    const suscriptor = await Suscriptor.findById( id )
+                           
+
+    res.json( suscriptor );
+
+}
+
+
+*/
+
+
+
